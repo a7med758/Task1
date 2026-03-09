b35bf32f052cb79349e9779ae202664c9841e0a8
a7med758 <158524512+a7med758@users.noreply.github.com>
2026-03-09 14:44:37 +0200

    Update node.py

--- scripts/node.py
+++ scripts/node.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python3
 import random 
+import itertools
 import rospy
 from Task1.srv import Service1, Service1Response
 
+all=list(itertools.product(range(0,9),repeat=2))
 
+unipu=random.sample(all,5)
+
+
+
+unipux,unipuy=zip(*(unipu))
+
+
+unido=random.sample(all,5)
+
+unidox,unidoy=zip(*(unido))
 
  # generate tasks
 
@@ -27,31 +39,25 @@ IDdo = [ 0,2,  4, 6,  8]
 PU = 1  # Initial value for PU
 DO = 0  # Initial value for DO
 
-
-
-for ID in IDs:
-    if PU == 1:  # Check if PU is 1
-       
-        x = random.randint(0, 8)
-        y = random.randint(0, 8)
-            
+k=0
+for ID in IDpu:
     
-        TS.append(TASK(ID, PU, DO, x, y))  # Create a TASK object and add it to TS
-        PU = 0  # Switch PU to 0
-        DO = 1  # Switch DO to 1
-        
-        
+    x=unipux[k]
+    y=unipuy[k]
+    TS.append(TASK(ID, PU, DO, x, y))  # Create a TASK object and add it to TS
+    k=k+1
 
-    else:  # If PU is 0
-        # Ensure that the random values for x and y are not the same as the ones in the if block
-        
-        x = random.randint(0, 8)
-        y = random.randint(0, 8)
-            
-        
-        TS.append(TASK(ID, PU, DO, x, y))  # Create a TASK object and add it to TS
-        DO = 0  # Switch DO to 0
-        PU = 1  # Switch PU to 1
+PU = 0  
+DO = 1 
+
+k=0
+for ID in IDdo:
+    
+    x=unidox[k]
+    y=unidoy[k]
+    TS.append(TASK(ID, PU, DO, x, y))  # Create a TASK object and add it to TS
+    k=k+1
+
 
 
 
@@ -62,41 +68,29 @@ def handle_calac(req):
    else:  
     if req.id == -1:
         # If this is the first task, return a random task from TS and remove it
-        i = random.choice(IDs)  
+        i = random.choice(IDpu)  
         
         
 
     else:
         #######// add log info for completed later //#######
-        # Loop to find a valid task with different PU or DO
+       
         if TS[req.id].PU == 1 :
            
-            for i in IDdo:   # Pick a new index
+            for i in IDdo:   # Pick up location not equal the drop off
                 if (TS[req.id].x != TS[i].x and TS[req.id].y != TS[i].y):
                     break
-                elif i==max(IDdo):
-                    while True:
-                        x = random.randint(0, 8)
-                        y = random.randint(0, 8)
-                        if x != TS[req.id].x and y != TS[req.id].y:  # Ensure x and y are not equal to the previous ones
-                            break
-                    TS[i]=TASK(i, 0, 1, x, y)
+                
                 IDdo.remove(i)
 
 
         else:
            
-            for i in IDpu:   # Pick a new  index with diffrent location
+            for i in IDpu:   
                 if (TS[req.id].x != TS[i].x and TS[req.id].y != TS[i].y):
                     break
-                elif i==max(IDpu): # all locations are invalid so generate new one and replace it 
-                    while True:
-                        x = random.randint(0, 8)
-                        y = random.randint(0, 8)
-                        if x != TS[req.id].x and y != TS[req.id].y:  
-                            break
-                    TS[i]=TASK(i, 0, 1, x, y)
-                    IDpu.remove(i)
+                
+                IDpu.remove(i)
         
         
         
