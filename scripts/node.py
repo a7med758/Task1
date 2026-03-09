4dd172c805e473b09805484164801e9399b882cb
a7med758 <158524512+a7med758@users.noreply.github.com>
2026-03-09 10:22:58 +0200

    Update node.py

--- scripts/node.py
+++ scripts/node.py
@@ -3,9 +3,8 @@ import random
 import rospy
 from Task1.srv import Service1, Service1Response
 
-if __name__=='__main__':
-    rospy.init_node("task")
-    
+
+
  # generate tasks
 
 class TASK:
@@ -19,33 +18,36 @@ class TASK:
 
 
 TS = []  # List to store the TASK objects
-IDs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+IDs = [0,1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+IDpu = [1, 3,  5,  7,  9]
+
+IDdo = [ 0,2,  4, 6,  8]
 
 PU = 1  # Initial value for PU
 DO = 0  # Initial value for DO
 
-# Variables to store x and y values from the if block for comparison
-prev_x, prev_y = None, None
+
 
 for ID in IDs:
     if PU == 1:  # Check if PU is 1
-        x = random.randint(0, 8)  # Random integer between 0 and 8
-        y = random.randint(0, 8)  # Random integer between 0 and 8
+       
+        x = random.randint(0, 8)
+        y = random.randint(0, 8)
+            
     
         TS.append(TASK(ID, PU, DO, x, y))  # Create a TASK object and add it to TS
         PU = 0  # Switch PU to 0
         DO = 1  # Switch DO to 1
         
-        # Store the random x and y values for later comparison
-        prev_x, prev_y = x, y
+        
 
     else:  # If PU is 0
         # Ensure that the random values for x and y are not the same as the ones in the if block
-        while True:
-            x = random.randint(0, 8)
-            y = random.randint(0, 8)
-            if x != prev_x or y != prev_y:  # Ensure x and y are not equal to the previous ones
-                break
+        
+        x = random.randint(0, 8)
+        y = random.randint(0, 8)
+            
         
         TS.append(TASK(ID, PU, DO, x, y))  # Create a TASK object and add it to TS
         DO = 0  # Switch DO to 0
@@ -54,28 +56,68 @@ for ID in IDs:
 
 
 def handle_calac(req):
-    l = len(TS)
-    i = random.choice(TS)  # Ensure valid index range (0 to l-1)
-
-    if req.id == 0:
-        # If request ID is 0, return a random task from TS and remove it
-        response=Service1Response(task.PU, task.DO, task.ID, task.x, task.y)
-        task = TS[i]
-        TS.remove(task)  # Remove task 
-        return response
+    
+   if not IDs:
+     response=Service1Response(0, 0, 0, -1, -1) # no more tasks
+   else:  
+    if req.id == -1:
+        # If this is the first task, return a random task from TS and remove it
+        i = random.choice(IDs)  
+        
+        
 
     else:
+        #######// add log info for completed later //#######
         # Loop to find a valid task with different PU or DO
-        while TS[req.id].PU == TS[i].PU or TS[req.id].DO == TS[i].DO:
-            i = random.choice(TS)  # Pick a new random index
+        if TS[req.id].PU == 1 :
+           
+            for i in IDdo:   # Pick a new index
+                if (TS[req.id].x != TS[i].x and TS[req.id].y != TS[i].y):
+                    break
+                elif i==max(IDdo):
+                    while True:
+                        x = random.randint(0, 8)
+                        y = random.randint(0, 8)
+                        if x != TS[req.id].x and y != TS[req.id].y:  # Ensure x and y are not equal to the previous ones
+                            break
+                    TS[i]=TASK(i, 0, 1, x, y)
+                IDdo.remove(i)
 
-        # Once a valid task is found, remove it and return the response
-        task = TS[i]
-        response=Service1Response(task.PU, task.DO, task.ID, task.x, task.y)
-        TS.remove(task)  # Remove the task 
-        return response
+
+        else:
+           
+            for i in IDpu:   # Pick a new  index with diffrent location
+                if (TS[req.id].x != TS[i].x and TS[req.id].y != TS[i].y):
+                    break
+                elif i==max(IDpu): # all locations are invalid so generate new one and replace it 
+                    while True:
+                        x = random.randint(0, 8)
+                        y = random.randint(0, 8)
+                        if x != TS[req.id].x and y != TS[req.id].y:  
+                            break
+                    TS[i]=TASK(i, 0, 1, x, y)
+                    IDpu.remove(i)
+        
+        
+        
+        
+        
+    task = TS[i]
+    IDs.remove(i)  # Remove the task
+    # Once a valid task is found, remove it and return the response
+    response=Service1Response(task.PU, task.DO, task.ID, task.x, task.y)
+         
+    return response
     
 
-rospy.Service('task',Service1, handle_calac)
 
-rospy.spin()
+def main():
+    rospy.init_node("task")
+    rospy.Service('task',Service1, handle_calac)
+
+    rospy.spin()
+
+
+if __name__=='__main__':
+    
+    main()
